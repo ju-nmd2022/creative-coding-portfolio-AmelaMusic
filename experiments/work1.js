79e730d6a3218b027f20bf40d92f3411c7c321dc
AmelaMusic <muam21gk@student.ju.se>
2024-08-29 11:25:11 +0200

    Vera molnar art commit

--- experiments/work1.js
+++ experiments/work1.js
@@ -1,7 +1,7 @@
 function setup() {
-  createCanvas(1000, 800);
+  createCanvas(1000, 600);
   background(255);
-  strokeWeight(2);
+  strokeWeight(1);
   drawLines();
   noLoop();
 }
@@ -9,10 +9,20 @@ function setup() {
 function drawLines() {
   const numberOfLines = 15;
   const lineSpacing = height / (numberOfLines + 1);
+  const lineLength = 200;
 
   for (let i = 0; i < numberOfLines; i++) {
     let y = (i + 1) * lineSpacing;
-    line(100, y, 900, y);
+
+    let offset = random(-60, 60);
+    let startX = 100 + offset;
+    let endX = 900 + offset;
+
+    if (endX < startX) {
+      endX = startX + random(1, 10);
+    }
+
+    line(startX, y, endX, y);
   }
 }
 
