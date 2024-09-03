a9f975b1ae9db6dad33f0943647da9dcc121eeb4
AmelaMusic <muam21gk@student.ju.se>
2024-09-03 10:33:12 +0200

    Update work 3 with flow field lecture example

--- experiments/work3.js
+++ experiments/work3.js
@@ -1,9 +1,43 @@
 function setup() {
     createCanvas(1000,600);
-    background(255);
     noLoop();  
-    drawCircularGrid();
 }
+
+  function draw() {
+    background(255); 
+    drawFlowField(); 
+    drawCircularGrid(); 
+  }
+
+  function drawFlowField() {
+    const fieldSize = 50; 
+    const fieldSizeHalf = fieldSize / 2;
+    const maxCols = Math.ceil(width / fieldSize);
+    const maxRows = Math.ceil(height / fieldSize);
+    const divider = 2;
+  
+    for (let x = 0; x < maxCols; x++) {
+        for (let y = 0; y < maxRows; y++) {
+          const padding = 10;
+          const value = noise(x / divider, y / divider) * Math.PI * 2;
+          push();
+          translate(x * fieldSize + fieldSizeHalf, y * fieldSize + fieldSizeHalf);
+          rotate(value);
+          strokeWeight(4);
+          fill(0, 0, 0);
+          line(-fieldSizeHalf + padding, 0, fieldSizeHalf - padding, 0);
+          triangle(
+            fieldSizeHalf - padding,
+            0,
+            fieldSizeHalf - padding * 2,
+            padding,
+            fieldSizeHalf - padding * 2,
+            -padding
+          );
+          pop();
+      }
+    }
+  }
 
 function drawCircularGrid() {
     const centerX = width / 2;
